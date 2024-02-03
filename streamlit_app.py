780e9ed47b946e421536bf3da51e4880f609c2dc
adil.serroukh <larache_adil@hotmail.com>
2024-02-04 00:20:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,52 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+import os
 import streamlit as st
+import subprocess
+from streamlit_monaco import st_monaco
+from diagrams import Diagram
 
+DIAGRAM_INSTRUCTIONS = """
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+
+with Diagram("Grouped Workers", show=False, direction="TB"):
+    ELB("lb") >> [EC2("worker1"),
+                  EC2("worker2"),
+                  EC2("worker3"),
+                  EC2("worker4"),
+                  EC2("worker5")] >> RDS("events")
 """
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.header("Chat Window")
+content = st_monaco(
+    value=DIAGRAM_INSTRUCTIONS,
+    height="200px",
+    language="python",
+    lineNumbers=True,
+    minimap=False,
+    theme="vs-dark",
+)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if st.button("Generate Diagram"):
+    # Delete image if exists
+    try:
+        os.remove("grouped_workers.png")
+    except FileNotFoundError:
+        print(f"File grouped_workers.png not found.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    with st.spinner("Generating ..."):
+        process = subprocess.run(['python', "-c", content], capture_output=True)
+        if process.returncode == 0:
+            st.image('grouped_workers.png')
+            # Dowlnoad the file
+            with open("grouped_workers.png", "rb") as file:
+                btn = st.download_button(
+                    label="Download Diagram",
+                    data=file,
+                    file_name="grouped_workers.png",
+                    mime="image/png"
+                )
+        else:
+            st.write(process.stderr.decode("utf-8"))
